58db9ab0a5d7210fd5fe535d7d2773febab3ba19
Jonathan B. Coe <jonathanbcoe@gmail.com>
2018-06-27 17:24:23 +0100

    Add dotnet-new console project VariablesDemo

---
+++ VariablesDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 39; // For now.
+            int x = 5;
+            double myHourlyRate = 150.00;
+            var myName = "Jonathan Coe";
+
+            Console.WriteLine($"my age: {myAge}, x:{x}, myHourlyRate:{myHourlyRate}, myName:{myName}");
+
+            var myLambda = (int myAge, int x, double myHourlyRate, string myName) => Console.WriteLine($"my age: {myAge}, x:{x}, myHourlyRate:{myHourlyRate}, myName:{myName}");
+        }
+    }
+}
