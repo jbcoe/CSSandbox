cc2d1f2456918a670641d5f193ce5b0f1089644d
Jonathan B. Coe <jonathanbcoe@gmail.com>
2018-06-27 17:22:28 +0100

    Init

---
+++ newProject.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eux
+
+ROOT=$(cd $(dirname $0); pwd)
+
+PROJECT="$ROOT"/"$1"
+
+mkdir "$PROJECT"
+(cd "$PROJECT"; 
+ dotnet new console; 
+ git init; 
+ echo "$ROOT"/gitignore > .gitignore; 
+ git add -A; 
+ git commit -m 'Init')
